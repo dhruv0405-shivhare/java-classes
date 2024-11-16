7c4c8e850319a9b9c4e4d980ce41c5603d02b97c
Dhruv <dshivhare661@gmail.com>
2024-11-16 14:28:58 +0530

    import export

---
+++ modules/modules.js
@@ -0,0 +1,9 @@
+import sum from './sum.js'
+
+console.log(sum(10,6))
+
+import {a,b} from './ex.js'
+
+console.log("a function")
+
+console.log("b function")
---
+++ modules/sum.js
@@ -0,0 +1,9 @@
+function sum(a,b){
+    return a+b
+
+
+}
+
+export default sum;
+
+// jb tum default ko import krte ho tb uska naam change kr skte h lekin sirf jb ek fuction ho tb change kr skte .
